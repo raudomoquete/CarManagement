a546931c7ff67b33f51d03da730733c63cb7dca3
Raudo Moquete <raudo.moquete@rhodium.com.do>
2023-03-28 10:47:30 -0400

    Automapper profile added

---
+++ CarMgmt.Core/DTOs/StatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarMgmt.Core
+{
+	public class StatusDto
+	{
+		public int Id { get; set; }
+
+		public string EnumName { get; set; } = null!;
+	}
+}
---
+++ CarMgmt.Infrastructure/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarMgmt.Core;
+
+namespace CarMgmt.Infrastructure
+{
+	public class AutoMapperProfile : Profile
+	{ 
+		public AutoMapperProfile()
+		{
+			CreateMap<Vehicle, VehicleDto>().ReverseMap();
+
+			CreateMap<Model, ModelDto>().ReverseMap();
+
+			CreateMap<Brand, BrandDto>().ReverseMap();
+
+			CreateMap<Brand, BrandDto>().ReverseMap();
+
+			CreateMap<Status, StatusDto>().ReverseMap();
+
+		}
+	}
+}
